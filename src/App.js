151d1a23d71472aa85df56d579da03e7324e2e1b
stephaneeh <stephaneehayden@gmail.com>
2024-05-12 16:07:56 +1000

    Commit dark theme toggle code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,45 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import "./style/app.css";
 
-function App() {
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Switch } from "@mui/material";
+
+export default function App() {
+  // state to manage the dark mode
+  const [toggleDarkMode, setToggleDarkMode] = useState(true);
+
+  // function to toggle the dark mode as true or false
+  const toggleDarkTheme = () => {
+    setToggleDarkMode(!toggleDarkMode);
+  };
+
+  // applying the primary and secondary theme colors
+  const darkTheme = createTheme({
+    palette: {
+      mode: toggleDarkMode ? "dark" : "light", // handle the dark mode state on toggle
+      primary: {
+        main: "#90caf9",
+      },
+      secondary: {
+        main: "#131052",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <div className="themeToggle">
+          <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} />
+        </div>
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/components/Topbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// import LightModeSharpIcon from "@mui/icons-material/LightModeSharp";
+// import NightlightRoundSharpIcon from "@mui/icons-material/NightlightRoundSharp";
+// import FormGroup from "@mui/material/FormGroup";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Switch from "@mui/material/Switch";
+
+const Topbar = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+      }}
+    >
+      <div>
+        <p>Hello Steph</p>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
